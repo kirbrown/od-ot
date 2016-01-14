7ef0f353ccc150e091f204a2c313c07d01e65ed8
Kirill <kirillbrown@gmail.com>
2016-01-14 23:50:57 +0600

    Add 'Signing up' fixture text, fix some staff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+
   resources :todo_lists do
     resources :todo_items do
       member do
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
       end
     end
   end
+
   root 'todo_lists#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
