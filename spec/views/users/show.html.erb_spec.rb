ca525d9a7038c6e68d97569393ee8260ad4d93f9
Kirill <kirillbrown@gmail.com>
2016-01-14 23:38:15 +0600

    Add new email validation, fix fields

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -3,18 +3,20 @@ require 'rails_helper'
 RSpec.describe "users/show", type: :view do
   before(:each) do
     @user = assign(:user, User.create!(
-      :first_name => "First Name",
-      :last_name => "Last Name",
-      :email => "Email",
-      :password_digest => "Password Digest"
+      :first_name => "MyString",
+      :last_name => "MyString",
+      :email => "mystring@mail.com",
+      :password => "MyString",
+      :password_confirmation => "MyString"
     ))
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/First Name/)
-    expect(rendered).to match(/Last Name/)
-    expect(rendered).to match(/Email/)
-    expect(rendered).to match(/Password Digest/)
+    expect(rendered).to match(/First name:/)
+    expect(rendered).to match(/Last name:/)
+    expect(rendered).to match(/Email:/)
+    expect(rendered).to match(/Password:/)
+    expect(rendered).to match(/Password confirmation:/)
   end
 end
