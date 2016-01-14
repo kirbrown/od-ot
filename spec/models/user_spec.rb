3ee25a6ef1eda7af0bb0a9e200e59823ddccf22a
Kirill <kirillbrown@gmail.com>
2016-01-14 23:01:04 +0600

    Add validation tests to User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  let(:valid_attributes) {
+    {
+      first_name: "John",
+      last_name: "Doe",
+      email: "john@doe.com"
+    }
+  }
+
+  context "validations" do
+    let(:user) { User.new(valid_attributes) }
+
+    before do
+      User.create(valid_attributes)
+    end
+
+    it "requires an email" do
+      expect(user).to validate_presence_of(:email)
+    end
+
+    it "requires a unique email" do
+      expect(user).to validate_uniqueness_of(:email)
+    end
+
+    it "requires a unique email (case insensitive)" do
+      user.email = "JOHN@DOE.COM"
+      expect(user).to validate_uniqueness_of(:email)
+    end
+  end
+
+  describe "#downcase_email" do
+    it "makes the email attribute lower case" do
+      user = User.new(valid_attributes.merge(email: "JOHN@DOE.COM"))
+      expect { user.downcase_email }.to change{ user.email }.
+        from("JOHN@DOE.COM").
+        to("john@doe.com")
+    end
+
+    it "downcases an email before saving" do
+      user = User.new(valid_attributes)
+      user.email = "JOHN@DOE.COM"
+      expect(user.save).to be_truthy
+      expect(user.email).to eq("john@doe.com")
+    end
+  end
 end
