129f75cee2f97615b51ef48a5fe10d593620448b
Kirill <kirillbrown@gmail.com>
2016-01-18 14:43:39 +0600

    Add the 'password reset' controller

---
+++ spec/controllers/password_resets_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe PasswordResetsController, type: :controller do
+  describe 'GET new' do
+    it 'renders the new template' do
+      get :new
+      expect(response).to render_template('new')
+    end
+  end
+
+  describe 'POST create' do
+    context 'with a valid user and email' do
+      let(:user) { create(:user) }
+
+      it 'finds the user' do
+        expect(User).to receive(:find_by).with(email: user.email).and_return(user)
+        post :create, email: user.email
+      end
+
+      it 'generates a new password token' do
+        expect{ post :create, email: user.email; user.reload }.to change{ user.password_reset_token }
+      end
+    end
+  end
+end
