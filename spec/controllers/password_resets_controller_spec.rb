7bcc72ea66cfb8e8344905eb81964371317ca0a6
Kirill <kirillbrown@gmail.com>
2016-01-18 15:12:54 +0600

    Add email notifier

--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -20,6 +20,10 @@ RSpec.describe PasswordResetsController, type: :controller do
       it 'generates a new password token' do
         expect{ post :create, email: user.email; user.reload }.to change{ user.password_reset_token }
       end
+
+      it 'sends a psssword reset email' do
+        expect{ post :create, email: user.email }.to change(ActionMailer::Base.deliveries, :size)
+      end
     end
   end
 end
