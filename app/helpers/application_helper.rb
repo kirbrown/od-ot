94c679d1fb692aa3c538495314a5f492a25dd93c
Kirill <kirillbrown@gmail.com>
2016-02-01 12:52:50 +0600

    Add global title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,16 @@
 module ApplicationHelper
+
+  def title(title)
+    content_for(:title) { "#{title} | " }
+  end
+
   def new_item_link
     if @todo_list && !@todo_list.new_record?
-      text = "Todo Item"
-      path = new_todo_list_todo_item_path(@todo_list)
+      text, path = "Todo Item", new_todo_list_todo_item_path(@todo_list)
     else
-      text = "Todo List"
-      path = new_todo_list_path
+      text, path = "Todo List", new_todo_list_path
     end
     link_to "Add #{text}", path, class: "icon-new float-right hide-text"
   end
+
 end
