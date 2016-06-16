e4c76382874f2fdf8ff85ccaef21b2c819e7a711
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-06-16 14:32:52 +0600

    Email todo lists

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,7 +3,14 @@ class Notifier < ApplicationMailer
   def password_reset(user)
     @user = user
     mail(to: "#{user.first_name} #{user.last_name} <#{user.email}>",
-         subject: 'Reset your password')
+         subject: 'Reset your password.')
+  end
+
+  def todo_list(todo_list, destination)
+    @user = todo_list.user
+    @todo_list = todo_list
+    mail(to: "#{@user.first_name} #{@user.last_name} <#{destination}>",
+         subject: "#{@user.first_name} #{@user.last_name} sent you a todo list.")
   end
 
 end
