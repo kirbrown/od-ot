2bdfcff56dae89e82b265b6edae649a7ed922556
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-06-17 21:29:06 +0600

    Redirect to todo lists if logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
 
   def home
-    render layout: 'home'
+    if logged_in?
+      redirect_to todo_lists_path
+    else
+      render layout: 'home'
+    end
   end
 
 end
