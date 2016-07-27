f0d08eae7748138ed12bc3abe7f6398c8619c376
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-07-27 12:44:37 +0600

    Add #delete action

--- app/controllers/api/todo_lists_controller.rb
+++ app/controllers/api/todo_lists_controller.rb
@@ -1,4 +1,5 @@
 class Api::TodoListsController < ApplicationController
+  before_action :find_todo_list, only: [:show, :destroy]
   skip_before_action :verify_authenticity_token
 
   def index
@@ -6,31 +7,42 @@ class Api::TodoListsController < ApplicationController
   end
 
   def show
-    list = TodoList.find(params[:id])
-    render json: list
+    render json: @list
   end
 
   def create
     list = TodoList.new(list_params)
     if list.save
       render json: {
-          status: 200,
-          message: 'Successfully created To-do List',
-          todo_list: list
+        status: 200,
+        message: 'Successfully created To-do List.',
+        todo_list: list
       }.to_json
     else
       render json: {
-          status: 500,
-          message: list.errors,
-          todo_list: list
+        status: 500,
+        message: list.errors,
+        todo_list: list
       }.to_json
     end
+  end
+
+  def destroy
+    @list.destroy
+    render json: {
+      status: 200,
+      messsage: 'Successfully daleted To-do List.'
+    }.to_json
   end
 
   private
 
   def list_params
     params.require(:todo_list).permit(:title)
+  end
+
+  def find_todo_list
+    @list = TodoList.find(params[:id])
   end
 
 end
