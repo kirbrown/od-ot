caa53193259efa55c02d28d933c5b6cf67a3f936
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-07-27 15:45:49 +0600

    Add basic authentication

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,3 +1,11 @@
 class Api::BaseController < ApplicationController
   skip_before_action :verify_authenticity_tokens
+  protect_from_forgery with: :null_session
+  before_action :authenticate
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |user, password|
+      user == 'admin' && password == 'password'
+    end
+  end
 end
