a465d61cb27dc5c530c991999cdee5650ab20f5b
Kirill Bobrov <k1r8r0wn@gmail.com>
2016-07-27 14:49:36 +0600

    Add #update & #delete actions to todo_items_controller

--- app/controllers/api/todo_items_controller.rb
+++ app/controllers/api/todo_items_controller.rb
@@ -2,6 +2,7 @@ class Api::TodoItemsController < ApplicationController
 
   skip_before_action :verify_authenticity_token
   before_action :find_todo_list
+  before_action :find_todo_item, only: [:update, :destroy]
 
   def create
     item = @list.todo_items.new(item_params)
@@ -19,6 +20,28 @@ class Api::TodoItemsController < ApplicationController
     end
   end
 
+  def update
+    if @item.update(item_params)
+      render status: 200, json: {
+          message: 'Successfully updated To-do Item.',
+          todo_list: @list,
+          todo_item: @item
+      }.to_json
+    else
+      render status: 422, json: {
+          message: 'To-do Item update is failed.',
+          errors: @item.errors
+      }.to_json
+    end
+  end
+
+  def destroy
+    @item.destroy
+    render status: 200, json: {
+        messsage: 'Successfully deleted To-do Item.'
+    }.to_json
+  end
+
   private
 
   def item_params
@@ -27,6 +50,10 @@ class Api::TodoItemsController < ApplicationController
 
   def find_todo_list
     @list = TodoList.find(params[:todo_list_id])
+  end
+
+  def find_todo_item
+    @item = @list.todo_items.find(params[:id])
   end
 
 end
