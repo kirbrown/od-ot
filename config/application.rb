b275066955d5a98d7b6b19574059a6a5658681f1
Kirill Bobrov <k1r8r0wn@gmail.com>
2017-03-31 03:02:03 +0600

    Add more fixes
    * according to new rails version

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,6 @@ module Odot
     config.i18n.default_locale = :en
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
     config.action_view.field_error_proc = Proc.new {|html, instance| html}
 
     # Halting Callback Chains via throw(:abort)
