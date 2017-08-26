e0af28b3d50f868fa8b44eba7fea6a19b20f9129
Kirill Bobrov <k1r8r0wn@gmail.com>
2017-08-26 14:27:10 +0600

    Remove RBP warnings

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,11 +1,9 @@
 class TodoItem < ApplicationRecord
-
   belongs_to :todo_list
 
   validates :content, presence: true, length: { minimum: 2 }
 
   scope :complete, -> { where('completed_at is not null') }
-  scope :incomplete, -> { where(completed_at: nil) }
 
   def completed?
     !completed_at.blank?
@@ -18,5 +16,4 @@ class TodoItem < ApplicationRecord
       update_attribute(:completed_at, Time.now)
     end
   end
-
 end
