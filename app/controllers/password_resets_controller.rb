d1e6be7496bd992eef4ffa4fce91da570247c9d3
Kirill Bobrov <k1r8r0wn@gmail.com>
2017-10-01 00:18:44 +0600

    Fix rubocop offenses

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -24,7 +24,7 @@ class PasswordResetsController < ApplicationController
   end
 
   def update
-    if @user && @user.update_attributes(user_params).present?
+    if @user&.update_attributes(user_params).present?
       @user.update_attribute(:password_reset_token, nil)
       session[:user_id] = @user.id
       redirect_to todo_lists_path, success: 'Password updated.'
