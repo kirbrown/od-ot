ca96e2eff66354af4fd56e3d360ff64b2b27c79c
Kirill Bobrov <k1r8r0wn@gmail.com>
2020-04-11 23:26:39 +0300

    Add more changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'https://od-ot.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
